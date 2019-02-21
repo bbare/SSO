28945e8526ec5c7c1f9171edbd1658aa14b0a172
ChrisMeyer7088 <chris_meyer_2@hotmail.com>
2019-02-20 18:35:17 -0800

    Fixed Dev project with merge conflicts

--- Backend/Dev/Program.cs
+++ Backend/Dev/Program.cs
@@ -1,13 +1,10 @@
 ﻿using ManagerLayer;
 using ManagerLayer.UserManagement;
 using System;
-<<<<<<< HEAD
 using DataAccessLayer.Database;
 using ServiceLayer.Services;
 using DataAccessLayer.Models;
 using UnitTesting;
-=======
->>>>>>> 9de364cebb612877c7f3127cd8a6d9c1f7839428
 
 namespace Program
 {
@@ -15,7 +12,6 @@ namespace Program
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
             var tu = new TestingUtils();
             var newUser = tu.CreateUserObject();
             Console.WriteLine(newUser.Email);
@@ -25,17 +21,6 @@ namespace Program
                 us.CreateUser(_db, newUser);
                 _db.SaveChanges();
             }
-=======
-
-            // create user
-            // the business req's to crete a user should go in the manger
-            //  - call services to create user and add to the database
-
-//            UserManagementManager umm = new UserManagementManager();
-  //          Guid guid = new Guid("f8d0c634-159e-4e8a-a561-19bc118a1b49");
-    //        UserManager userManger = new UserManager();
-      //      umm.DeleteUser(guid);
->>>>>>> 9de364cebb612877c7f3127cd8a6d9c1f7839428
             Console.ReadKey();
         }
     }
