7cfca2f610d0c90540f91048940e20afc0392a44
Winn <winnmoo@gmail.com>
2019-02-23 17:04:32 -0800

    Created controller and unit testing files

---
+++ Backend/UnitTesting/ResetPasswordUT.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTesting
+{
+    [TestClass]
+    class ResetPasswordUT
+    {
+        [TestMethod]
+        public void createToken_Pass()
+        {
+
+        }
+
+        [TestMethod]
+        public void createToken_Fail()
+        {
+
+        }
+
+
+        public void expiredToken_Pass()
+        {
+
+        }
+
+        public void expiredToken_Fail()
+        {
+
+        }
+
+        public void retrieveSecurityQuestions_Pass()
+        {
+
+        }
+
+        public void retrieveSecurityQuestions_Fail()
+        {
+
+        }
+
+    }
+}
