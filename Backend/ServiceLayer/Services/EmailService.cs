c8af971e9333ba6580f6ee6f3677e5b7eea7abd7
Winn <winnmoo@gmail.com>
2019-02-24 13:18:53 -0800

    Created necessary files and added some functions

---
+++ Backend/ServiceLayer/Services/EmailService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MailKit;
+using MimeKit;
+
+namespace ServiceLayer.Services
+{
+    class EmailService
+    {
+
+        //TODO: Move to ResetService.cs
+        //Needs work
+        private const string resetPasswordBodyStringPart1 = "Hi, ";
+        private const string resetPasswordBodyStringPart2 = "You recently requested to reset your password for your KFC account, click the link below to reset it." +
+            "The URL is only valid for the next 5 minutes";
+        private const string resetPasswordBodyStringPart3 = "If you did not request to reset your password, please contact us by responding to this email."
+            + "Thanks, KFC Team";
+
+        //TODO: Move to ResetService.cs
+        //Send this email if the user exists in the system
+        public void sendResetMessageExists(string receiverName, string receiverEmail, string emailSubject,string resetPasswordURL)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Support", "support@kfcsso.com"));
+            message.To.Add(new MailboxAddress(receiverName, receiverEmail));
+            message.Subject = emailSubject;
+            //The "plain" parameter denotes that the message format type, can be switched to "html" if needed
+            message.Body = new TextPart("plain")
+            {
+                //TODO: Find a better way to create the message body
+                Text = resetPasswordBodyStringPart1 + receiverName + resetPasswordBodyStringPart2 + resetPasswordURL + resetPasswordBodyStringPart3
+            };
+        }
+
+        //Function to send an email
+        public void sendEmail(string receiverName, string receiverEmail, string emailSubject, string emailBody)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Support", "support@kfcsso.com"));
+            message.To.Add(new MailboxAddress(receiverName, receiverEmail));
+            message.Subject = emailSubject;
+            message.Body = new TextPart("plain")
+            {
+                Text = emailBody
+            };
+        }
+    }
+}
---
+++ Backend/ServiceLayer/Services/ResetService.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.Database;
+using DataAccessLayer.Models;
+using DataAccessLayer.Repositories;
+
+
+namespace ServiceLayer.Services
+{
+    class ResetService
+    {
+        //Variable for how long the token is supposed to be live
+        private const double TimeToExpire = 5;
+        //Constant for the password reset email subject line
+        private const string resetPasswordSubjectString = "KFC SSO Password Reset";
+
+        //Check DB for User
+        public bool checkUser(string userName)
+        {
+            bool userExists = false;
+
+            return userExists;
+        }
+
+
+
+        //Function to create token in the system
+        public string createToken(string userName)
+        {
+            //Gets current time, and adds the time to expire constant
+            DateTime expirationTime = DateTime.Now.AddMinutes(TimeToExpire);
+
+            byte[] time = BitConverter.GetBytes(expirationTime.ToBinary());
+            byte[] key = Guid.NewGuid().ToByteArray();
+
+            string token = Convert.ToBase64String(time.Concat(key).ToArray());
+
+            addTokenToDB(userName, token, expirationTime);
+
+            return token;
+        }
+
+        //Function to add the token to the database
+        public void addTokenToDB(string userName, string token, DateTime expirationTime)
+        {
+            //SQL Query to add 3 arguments to token table in DB
+            //sql query to disable the user
+        }
+
+        //Read the token given from the URL
+        public bool checkTokenIsValid(string token)
+        {
+            bool tokenValid = false;
+            byte[] data = Convert.FromBase64String(token);
+            DateTime when = DateTime.FromBinary(BitConverter.ToInt64(data, 0));
+
+            //Check to see if the token is valid
+            
+            //SQL Query to see if token exists in DB
+                //get the byte data from token in db
+                    //Compare the two's data to see if they're the same token
+
+            //Case where token hasn't expired
+            if(!(when < DateTime.Now))
+            {
+                tokenValid = true;
+                deleteTokenFromDB();
+                return tokenValid;
+            }
+            //Case where token has expired
+            else
+            {
+                deleteTokenFromDB();
+                return tokenValid;
+            }
+        }
+
+        public void deleteTokenFromDB()
+        {
+
+        }
+
+        public void sendResetEmailUserExists()
+        {
+
+        }
+        public void sendResetEmailUserDoesNotExist()
+        {
+
+        }
+    }
+}
