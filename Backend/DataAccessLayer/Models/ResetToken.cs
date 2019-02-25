d09af94725200eefaae6ce054a729e2c426ea3bc
WinnMoo <winnmoo@gmail.com>
2019-02-24 19:15:41 -0800

    Added the model for reset token, created a repository class for reset token

---
+++ Backend/DataAccessLayer/Models/ResetToken.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models
+{
+    class ResetToken
+    {
+        [Required, Key] 
+        public string resetToken { get; set; }
+        [Required, ForeignKey("User")]
+        public string userID { get; set; }
+        [Required]
+        public DateTime expirationTime { get; set; }
+    }
+}
