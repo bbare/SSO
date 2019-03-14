9b918c47cebb3fda7fd91e7961eaf22fe76a9248
Krystal Leon <krystalleon10@gmail.com>
2019-03-13 17:16:19 -0700

    remove axios interceptor

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -11,17 +11,3 @@ new Vue({
   render: h => h(App)
 }).$mount('#app')
 
-// Add a request interceptor
-axios.interceptors.request.use(function (config) {
-  return config;
-}, function (error) {
-  return Promise.reject(error);
-});
-
-// Add a response interceptor
-axios.interceptors.response.use(function (response) {
-  return response;
-}, function (error) {
-  alert(error.response.data);
-  return Promise.reject(error);
-});
