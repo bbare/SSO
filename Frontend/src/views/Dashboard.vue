1d798c85aaca26a9312767975b1d82bbcd21dce8
zanderlx <lsaplan@gmail.com>
2019-03-26 01:39:54 -0700

    Displays apps registered with the SSO

--- Frontend/src/views/Dashboard.vue
+++ Frontend/src/views/Dashboard.vue
@@ -1,61 +1,144 @@
 <template>
   <div id="portal">
-    <h1>This is the Portal</h1>
-
     <!-- Feel free to hardcode an app ID here until dashboard logic is complete -->
-    <button v-on:click="launch('70579CB0-3D33-47C3-8CAD-650FC4FFBBAC')">Fake App (hardcoded ID)</button>
+    <!-- <button v-on:click="launch('D146A971-593A-47E4-ABCB-8B3AAF686ECD')">Fake App (hardcoded ID)</button> -->
 
-    <v-alert
-      :value="error"
-      type="error"
-      transition="scale-transition"
-    >
-      {{error}}
-    </v-alert>
+    <v-card>
+      <h1 id="applicationPortal">Application Portal</h1>
+      <v-container fluid grid-list-md>
+        <v-layout row wrap>
+          <v-flex xs12 sm6 md4 lg3 v-for="(app, index) in applications" :key="index">
+            <v-card hover @mouseover="app.showInfo = true" @mouseleave="app.showInfo = false">
+              <v-icon
+                v-if="app.showInfo"
+                large
+                color="orange"
+                style="float: right"
+                @click="showInfo = true"
+              >info</v-icon>
+
+              <v-card-title primary-title @click="launchLoading = true">
+                <img src="https://www.freeiconspng.com/uploads/no-image-icon-15.png">
+                <div id="content">
+                  <h3 class="headline mb-0">
+                    <strong>{{ app.Title }}</strong>
+                  </h3>
+                </div>
+              </v-card-title>
+            </v-card>
+            <div v-if="launchLoading">
+              <Loading :dialog="launchLoading"/>
+            </div>
+            <div v-if="showInfo">
+              <AppInfo :dialog="showInfo" :appDetails="app" @exitAppInfo="exitAppInfo"/>
+            </div>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-card>
+
+    <v-alert :value="error" type="error" transition="scale-transition">{{error}}</v-alert>
   </div>
 </template>
 
 <script>
-import { signLaunch, submitLaunch } from '@/services/request';
+import Loading from "@/components/Dialogs/Loading.vue";
+import AppInfo from "@/components/Dialogs/AppInfo.vue";
+import { signLaunch, submitLaunch } from "@/services/request";
+import axios from "axios";
 
 export default {
-  data () {
+  components: { Loading, AppInfo },
+  data() {
     return {
+      applications: [],
+      showInfo: false,
+      launchLoading: false,
       error: ""
+    };
+  },
+  watch: {
+    launchLoading(val) {
+      if (!val) return;
+      setTimeout(() => (this.launchLoading = false), 3000);
     }
   },
   methods: {
+    exitAppInfo(hideInfo) {
+      this.showInfo = hideInfo;
+    },
     launch(appId) {
       this.error = "";
 
-      signLaunch(appId).then(launchData => {
-        submitLaunch(launchData).then(launchResponse => {
-          window.location.href = launchResponse.redirectURL;
-        }).catch(err => {
+      signLaunch(appId)
+        .then(launchData => {
+          submitLaunch(launchData)
+            .then(launchResponse => {
+              window.location.href = launchResponse.redirectURL;
+            })
+            .catch(err => {
+              let code = err.response.status;
+
+              switch (code) {
+                case 500:
+                  this.error =
+                    "An unexpected server error occurred. Please try again momentarily.";
+                  break;
+                default:
+                  this.error =
+                    "An unexpected server error occurred. Please try again momentarily.";
+                  break;
+              }
+            });
+        })
+        .catch(err => {
           let code = err.response.status;
 
-          switch(code) {
+          switch (code) {
             case 500:
-              this.error = "An unexpected server error occurred. Please try again momentarily.";
+              this.error =
+                "An unexpected server error occurred. Please try again momentarily.";
               break;
             default:
-              this.error = "An unexpected server error occurred. Please try again momentarily.";
+              this.error =
+                "An unexpected server error occurred. Please try again momentarily.";
               break;
           }
-        })
-      }).catch(err => {
-        let code = err.response.status;
+        });
+    }
+  },
+  async mounted() {
+    await axios
+      .get("http://localhost:61348/api/applications")
+      .then(response => (this.applications = response.data));
 
-        switch(code) {
-          case 500:
-            this.error = "An unexpected server error occurred. Please try again momentarily.";
-            break;
-          default:
-            this.error = "An unexpected server error occurred. Please try again momentarily.";
-            break;
-        }
-      })
+    // Add attribute for displaying info icon
+    // Add attribute for editing app description
+    for (var i = 0; i < this.applications.length; i++) {
+      this.$set(this.applications[i], "showInfo", false);
+      this.$set(this.applications[i], "editDescription", false);
     }
   }
-}
+};
 </script>
+
+<style scoped>
+.v-card {
+  margin: 1em;
+}
+
+#applicationPortal {
+  padding: 1em;
+  font-size: 38px;
+  text-decoration: underline;
+}
+
+#content {
+  margin-left: 1em;
+}
+
+img {
+  width: 55px;
+  height: 55px;
+}
+</style>
