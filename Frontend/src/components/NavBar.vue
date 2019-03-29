beacfa903be9ba9b9f7d14a5a32c8fbc00275f67
cf2080 <cf2080@gmail.com>
2019-03-29 10:07:39 -0700

    Navbar Modification. Put v-if statement in register to hide register when user logs in

--- Frontend/src/components/NavBar.vue
+++ Frontend/src/components/NavBar.vue
@@ -6,11 +6,11 @@
     <v-spacer></v-spacer>
 
     <v-btn to="home" flat>Home</v-btn>
-    <v-btn to="register" flat>Register</v-btn>
+    <v-btn to="register" flat v-if="!isLoggedIn.isLogin">Register</v-btn>
     <v-btn to="about" flat>About</v-btn>
-    <v-btn to="add" flat>Register</v-btn>
-    <v-btn to="key" flat>Generate</v-btn>
-    <v-btn to="delete" flat>Delete</v-btn>
+    <v-btn to="add" flat >App Register</v-btn>
+    <v-btn to="key" flat>Generate Key</v-btn>
+    <v-btn to="delete" flat>App Delete</v-btn>
     <v-btn to="login" flat v-if="!isLoggedIn.isLogin">Login</v-btn>
     <v-menu
       offset-y
