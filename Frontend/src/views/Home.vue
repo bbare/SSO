4534053381e8e827d4879ee206ef27dfe3a006f5
Alfredo Vargas <vargas.alfredojr@gmail.com>
2019-04-03 16:19:10 -0700

    updated home page

--- Frontend/src/views/Home.vue
+++ Frontend/src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
-  <div>
-    <h1>Welcome to KFC SSO!</h1>
-  </div>
+  <v-flex xs12 sm6 offset-sm3>
+    <img src="@/assets/KFC_Logo_v2.png" id="logo"/>
+    <h1 align="center" id="text">Welcome to your single sign on portal!</h1>
+  </v-flex>
 </template>
 
 <script>
@@ -11,5 +12,15 @@ export default {
 </script>
 
 <style scoped>
-  
+#logo{
+  width: 100%;
+  height: 100%;
+  align: center;
+}
+
+#text{
+  align: center;
+  padding: 10px;
+  width: 100%;
+}
 </style>
